85c00dc0200843ef68dfb418f8db7a39291fa1c6
Harm <harmjanluth@gmail.com>
2015-10-30 14:00:43 +0100

    Added login

--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,20 @@
+//import * as hoodie from 'hoodie';
+
 (function () {
     'use strict';
 
+    const hoodie = new Hoodie();
+
+    document.querySelector('.form').onsubmit = function(e) {
+      e.preventDefault();
+
+      let email = document.querySelector('[name=email]').value;
+      let password = document.querySelector('[name=password]').value;
+
+      hoodie.account.signIn(email, password)
+        .done(function(){alert("welcome!")})
+        .fail(function(){alert("doei!")});
+    }
 
     console.log('jo!')
 })();
